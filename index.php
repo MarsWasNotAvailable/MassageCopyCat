e48d41eb7f000871c5dd24027a8d76a14c6cacb0
MarsWasNotAvailable <116720820+MarsWasNotAvailable@users.noreply.github.com>
2023-06-16 15:24:02 +0400

    base
    the structure is almost done, footer is missing.

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <script defer src="./script.js"></script>
+
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <nav id="NavigationBar">
+            <a id="Home" class="navigational" href="#"><img src="./cache/massage_logo_header-1.png" alt="company header logo"></a>
+
+            <div id="HamburgerMenu">
+                <!-- We are swapping between the Hamburger button and the UnorderedList responsively -->
+                <button id="HamburgerButton" class="navigational"><img src="./cache/icons_Hamburger.png" onclick="ToggleHamburger()"></button>
+                <ul id="HamLinks" class="Collapsed">   
+                    <li class="navigational"><a href="./index.html">Homepage</a></li>
+                    <li class="navigational"><a href="./articles.html">About Us</a></li>
+                    <li class="navigational"><a href="./contact.html">Page & Projects</a></li>
+                    <li class="navigational"><a href="./contact.html">Blog</a></li>
+                    <li class="navigational"><button>Contact</button></li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+
+    <section id="Welcome" class="ContentBoxes">
+        <img src="./cache/icon05.png" alt="">
+        <h1>Welcome</h1>
+        <h2>In beauty paradise</h2>
+        <p>In publishing and graphic design, lorem ipsum is common to cry</p>
+        <button>Book a massage</button>
+    </section>
+    
+    <section id="Services" class="ContentBoxes">
+        <h4>What we do</h4>
+        <h3>Our services</h3>
+        <hr>
+        <p>In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic elements of a document or</p>
+        <div>
+            <span>Professional Massage</span>
+            <span>Professional Manicure</span>
+            <span>Make-up Artist</span>
+        </div>
+    </section>
+
+    <section id="Uniques" class="ContentBoxes">
+        <h4>What makes us unique</h4>
+        <h3>Why to choose our salon</h3>
+        <p>In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic elements of a document or</p>
+        <div>
+            <span>
+                <ul> <li>Lorem Ipsum Dolor</li> </ul>
+                <ul> <li>Lorem Ipsum Dolor</li> </ul>
+                <ul> <li>Lorem Ipsum Dolor</li> </ul>
+            </span>
+            <span>
+                <ul> <li>Lorem Ipsum Dolor</li> </ul>
+                <ul> <li>Lorem Ipsum Dolor</li> </ul>
+                <ul> <li>Lorem Ipsum Dolor</li> </ul>
+            </span>
+            <img src="./cache/massage18.png" alt="" >
+        </div>
+    </section>
+
+    <section id="BookingBar" class="ContentBoxes">
+        <span>Book a massage today</span>
+        <button>Book a massage today</button>
+    </section>
+
+    <section id="Studio" class="ContentBoxes">
+        <h4>Introducing Massage Studio</h4>
+        <hr>
+        <p>In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic elements of a</p>
+        <p>Even though using “lorem ipsum” often arouses curiosity due to its resemblance to classical Latin, it is not intended to have meaning.</p>
+    </section>
+
+    <section id="Deserve" class="ContentBoxes">
+        <span>
+            <div class="Card">
+                <img src="./cache/icon08.png" alt="">
+                <h6>Top Privacy</h6>
+                <p>Lorem Ipsum is simply dummy text. Lorem Ipsum is simply dummy text corel ozel lorem.</p>
+            </div>
+            <div class="Card">
+                <img src="./cache/icon08.png" alt="">
+                <h6>Top Privacy</h6>
+                <p>Lorem Ipsum is simply dummy text. Lorem Ipsum is simply dummy text corel ozel lorem.</p>
+            </div>
+            <div class="Card">
+                <img src="./cache/icon08.png" alt="">
+                <h6>Top Privacy</h6>
+                <p>Lorem Ipsum is simply dummy text. Lorem Ipsum is simply dummy text corel ozel lorem.</p>
+            </div>
+            <div class="Card">
+                <img src="./cache/icon08.png" alt="">
+                <h6>Top Privacy</h6>
+                <p>Lorem Ipsum is simply dummy text. Lorem Ipsum is simply dummy text corel ozel lorem.</p>
+            </div>
+            <div class="Card">
+                <img src="./cache/icon08.png" alt="">
+                <h6>Top Privacy</h6>
+                <p>Lorem Ipsum is simply dummy text. Lorem Ipsum is simply dummy text corel ozel lorem.</p>
+            </div>
+        </span>
+        <div>
+            <img src="./cache/massage39.jpg" alt="">
+            <div>
+                <h4>You deserve your time!</h4>
+                <hr>
+                <p>In publishing and graphic design, lorem ipsum is common placeholder text used to demonstrate the graphic elements of a</p>
+                <p>Even though using “lorem ipsum” often arouses curiosity due to its resemblance to classical Latin, it is not intended to have meaning. Where text is visible in a document, people tend to focus on the textual content rather than upon overall presentation, so publishers use lorem h.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="Uniques" class="ContentBoxes">
+        <h4>magazine</h4>
+        <h3>Our beauty blog</h3>
+        <hr>
+        
+        <div>
+            <div class="Card">
+                <img src="./cache/massage21-400x250.jpg" alt="">
+                <h5>Best haircuts trends in 2020</h5>
+                <h6>by Richard Pruzek</h6>
+                <button>read more</button>
+            </div>
+            <div class="Card">
+                <img src="./cache/massage21-400x250.jpg" alt="">
+                <h5>Best haircuts trends in 2020</h5>
+                <h6>by Richard Pruzek</h6>
+                <button>read more</button>
+            </div>
+            <div class="Card">
+                <img src="./cache/massage21-400x250.jpg" alt="">
+                <h5>Best haircuts trends in 2020</h5>
+                <h6>by Richard Pruzek</h6>
+                <button>read more</button>
+            </div>
+        </div>
+    </section>
+
+    <section id="Uniques" class="ContentBoxes">
+        <h4>meet our team</h4>
+        <h3>Our awesome masseurs</h3>
+        <hr>
+        
+        <div>
+            <div class="Card">
+                <img src="./cache/massage18.png" alt="">
+                <h5>Amelie Schweinsteiger</h5>
+                <p>In publishing and graphic design, lorem ipsum is common</p>
+                <span>
+                    <button><img src="./cache/icons_socials_Facebook.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Twitter.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Instagram.png" alt=""></button>
+                </span>
+            </div>
+
+            <div class="Card">
+                <img src="./cache/massage37-1.png" alt="">
+                <h5>Annia Levanderr</h5>
+                <p>In publishing and graphic design, lorem ipsum is common</p>
+                <span>
+                    <button><img src="./cache/icons_socials_Facebook.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Twitter.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Instagram.png" alt=""></button>
+                </span>
+            </div>
+
+            <div class="Card">
+                <img src="./cache/massage35-1-1.png" alt="">
+                <h5>Nina Heitelberger</h5>
+                <p>In publishing and graphic design, lorem ipsum is common</p>
+                <span>
+                    <button><img src="./cache/icons_socials_Facebook.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Twitter.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Instagram.png" alt=""></button>
+                </span>
+            </div>
+
+            <div class="Card">
+                <img src="./cache/massage38.png" alt="">
+                <h5>Kiki Katzenmiller</h5>
+                <p>In publishing and graphic design, lorem ipsum is common</p>
+                <span>
+                    <button><img src="./cache/icons_socials_Facebook.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Twitter.png" alt=""></button>
+                    <button><img src="./cache/icons_socials_Instagram.png" alt=""></button>
+                </span>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
